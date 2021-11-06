fc975efc453eafc650c8d85a2a246eb6d6e1c099
Craig Jacobson <craig.crj@gmail.com>
2021-11-05 21:19:27 -0600

    Remove ; from landing page.

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -3,7 +3,7 @@
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
 
-  return <h1>Landing Page {currentUser ? 'Signed In as ' + currentUser.email : 'Signed Out'}</h1>;
+  return <h1>Landing Page {currentUser ? 'Signed In as ' + currentUser.email : 'Signed Out'}</h1>
 };
 
 export default LandingPage;
