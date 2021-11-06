8f0e2be24ef8cdfdae9646c65ef5add9131c5be6
Craig Jacobson <craig.crj@gmail.com>
2021-11-05 21:17:02 -0600

    Add header and signout.

--- ticketing/client/pages/_app.js
+++ ticketing/client/pages/_app.js
@@ -1,7 +1,39 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import ssrclient from '../api/ssr-client';
+import Header from '../components/header';
 
-const applyToComponent = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  pageProps.currentUser = currentUser;
+  const headerProps = { currentUser };
+  return (
+    <div>
+      <Header { ...headerProps } />
+      <Component { ...pageProps } />;
+    </div>
+  );
 };
 
-export default applyToComponent;
+AppComponent.getInitialProps = async (appContext) => {
+  let data = { currentUser: null };
+
+  try {
+    const client = ssrclient(appContext.ctx);
+    const response = await client.get('/api/users/currentuser');
+    data = response.data;
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    ...data
+  };
+};
+
+export default AppComponent;
--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,21 +1,9 @@
-import axios from 'axios';
-import ssrclient from '../api/ssr-client';
 
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
 
   return <h1>Landing Page {currentUser ? 'Signed In as ' + currentUser.email : 'Signed Out'}</h1>;
-};
-
-LandingPage.getInitialProps = async (context) => {
-  try {
-    const response = await ssrclient(context).get('/api/users/currentuser');
-    return response.data;
-  } catch (err) {
-    console.log(err.message);
-    return { currentUser: null };
-  }
 };
 
 export default LandingPage;
